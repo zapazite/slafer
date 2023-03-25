625a53d0cecea84a359f09d90065dba7b199049b
Maxime De Prins <rapasite@users.noreply.github.com>
2023-03-25 02:32:18 +0100

    Update Ctoyarrlang.c

--- Ctoyarrlang.c
+++ Ctoyarrlang.c
@@ -1,3 +1,5 @@
+//https://tio.run/##XVJtb@IwDP6eXxGBNsWpiyjstVmGppMmTWJibx8mekxCWcuKaIYg2xWq/vXjHAa3031JYvuJ/Ty2TTgxZrNpvqZZblM@EBbLVqsFqsreFyK3jue6rfILq/IggKpUdc324DtxjRlciz5fYZ@XUPX5Wg@FXMFArMJLi@vwcp7kI02G65HhfjId4bUPej@WEO@eGS@3jwO67AhWeq1q5lbzlErxpVt8GFd5MhYdzpNRLfvKqkJJp6imSukcCgvVWhfj2ezdCGblGSgqabVV9Y2nCBVBtPQFwcvck8u/qt2JKwzgTjwhk3TdYgg/vpWVXxrkUERA/3SJ67hUQ0EZvVIIPG9QrEgLM18Jnxt9Jdzy@McbUH@93@N3FN02t2L1455mBEp68Da3qh@@eWwllP9LWDHY5olULWSWjECArtr4gG28wie8xR/4iDe1evYDlQ6qVFPnnBaFliwIHPSku4g6p73T8xdJNpk6S4qXo5NeEZ8E0o1EivQPn4WDWBACXRKNNMsSH8uSk6AgCODu5YPg0T43xNLFqarvScUaqoFvA1Fnvp/3YicBvBYbRlrnvXi@IJqZaHDVgG/j4LWxF8wA6mKcW2FwCpV5Gy9kiXKJ8lM3mnkgQ2yogTh0aDXtziy1YpK6pVgCqCmjdXb7NfEj8I00eklpVbfzYnoumQYBrcXyNZ/kThjo@YGgHwibaxMendHsRekzm7eF@EQD0ItoAT5jE7dhetA5PLwXvlOg5h9UttEgupvNcStnjA7e5F3Gznk@kPz4r4cjj7CD3S9HizcJso0c84Cf8pBHXPJzxn6bbDaeLDdh0e1swl9/AA
+// From finooiigee (apl farm discord)
 #define O(n,x...);{for(int i=0;i<n;i++){x;}}
 #define P(F,f)F(L y,L x){L z=Z(*y)O(y->n,z->p[i]=y->t?z->t\
 =1,F(y->p[i],x):y->p[i]f x->p[i%x->n])y=z;}
